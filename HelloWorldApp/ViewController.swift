3cc73180e39f0e56f3e1b72b1d5ca3dee5741d67
Sawwaty <yezhovye@mac.com>
2021-01-31 22:48:50 +0300

    code was added

--- HelloWorldApp/ViewController.swift
+++ HelloWorldApp/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var showTextButton: UIButton!
+    @IBOutlet var labelTextLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        labelTextLabel.isHidden = true
+        showTextButton.layer.cornerRadius = 10
+        labelTextLabel.textColor =  .red
     }
 
-
+    @IBAction func showTextPressedButton() {
+        if labelTextLabel.isHidden {
+            labelTextLabel.isHidden.toggle()
+            showTextButton.setTitle(" Hide Text", for: .normal)
+        } else {
+            labelTextLabel.isHidden.toggle()
+            showTextButton.setTitle("Show text", for: .normal)
+        }
+    }
+    
 }
 
